ea299ee3a07957bba1bc030bda774f0d00c72281
vitorrf4 <vitorrf4@gmail.com>
2024-01-15 11:00:05 -0300

    render enquetes on html

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,3 +1,42 @@
+async function loadEnquetes() {
+    const enquetes = await fetch('http://localhost:3000/enquetes')
+        .then(async r => await r.json());
+
+    for (let enquete of enquetes) {
+        createEnqueteHTML(enquete);
+    }
+}
+
+function createEnqueteHTML(enquete) {
+    const container = document.getElementById("flex-container");
+    const div = document.createElement('div');
+    const table = document.createElement('table');
+
+    div.insertAdjacentHTML('afterbegin', `<p>${enquete.titulo}</p>`);
+
+    for (let opcao of enquete.opcoes) {
+        table.insertAdjacentHTML('afterbegin', `
+            <tr>
+                 <td><label for="opcao1">${opcao.titulo}</label></td>
+                 <td>${opcao.votos} votos</td> 
+                 <td><input type="radio" name="opcao" id="opcao_${opcao.id}"></td>
+            </tr>
+        `);
+    }
+    div.append(table);
+
+    div.insertAdjacentHTML('beforeend', `
+            <p>Inicio: ${enquete.dataInicio}</p>
+            <p>Fim: ${enquete.dataFim}</p>
+            <p>Status: ${enquete.status}</p>
+
+            <button>Votar</button>
+    `);
+    div.classList.add('enquetes-div');
+
+    container.appendChild(div);
+}
+
 async function saveEnquete() {
     const titulo = document.getElementById("titulo").value;
     const dataInicio = document.getElementById("dataInicio").value;
@@ -8,7 +47,7 @@ async function saveEnquete() {
         titulo: titulo,
         dataInicio: dataInicio,
         dataFim: dataFim,
-        opcoes: opcao
+        opcoes: [{titulo: opcao}]
     });
 
     console.log(enquete);
